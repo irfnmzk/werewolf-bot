177abd6ebbcd6d609623b5ed5398a1f1690374e5
fauzanmh <33658024+fauzanmh@users.noreply.github.com>
2022-06-06 21:26:21 +0700

    handle bot if join or left from a group

--- telegram/handler.go
+++ telegram/handler.go
@@ -2,6 +2,7 @@ package telegram
 
 import (
 	"log"
+	"os"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"github.com/irfnmzk/werewolf-arena/telegram/command"
@@ -10,6 +11,8 @@ import (
 func (tc *Client) handleCommand(msg *tgbotapi.Message, text string) {
 	command := command.NewCommand(tc.log, tc.bot, msg, tc.redisClient)
 	switch text {
+	case "greeting_join_group_or_channel":
+		command.GreetingJoinGroupOrChannel()
 	case "start", "mulai":
 		command.StartGame()
 	case "about":
@@ -40,4 +43,16 @@ func (tc *Client) handleCallback(cb *tgbotapi.CallbackQuery) {
 	}
 
 	tc.handleCommand(cb.Message, cb.Data)
+}
+
+func (tc *Client) handleLeftChat(msg *tgbotapi.Message) {
+	// Check if left chat member is bot werewolf
+	// and will send a message for the kicker
+	if msg.LeftChatMember.IsBot && msg.LeftChatMember.UserName == os.Getenv("USERNAME_BOT") {
+		message := tgbotapi.NewMessage(msg.From.ID, "Yah kak ko aku di keluarin dari grup, apakah aku ada salah atau kurang menyenangkan kak? Kalo iya, maaf ya kak kedepannya aku janji bakal terus memperbaiki diri dan semoga kakak memberikan aku kesempatan lagi untuk bermain bersama :)")
+		_, err := tc.bot.Send(message)
+		if err != nil {
+			log.Panic(err)
+		}
+	}
 }
